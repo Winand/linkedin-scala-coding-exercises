4d819ca696a42f7236db8ce364f77a17424a66b9
Makarov Andrey <winandfx@gmail.com>
2024-05-06 10:25:05 +0300

    place required part in a separate group

--- src/main/scala/RegExMatches.scala
+++ src/main/scala/RegExMatches.scala
@@ -1,8 +1,8 @@
 object RegExMatches {
   def snakeToCamel(string: String): String =
-    val pattern = "_[a-z]".r
+    val pattern = "_([a-z])".r
     pattern.replaceAllIn(string, matched =>
-      matched.group(0).substring(1).toUpperCase
+      matched.group(1).toUpperCase
     )
 
   @main def regex(): Unit = {
